e39d77c5f5e1945e4fc6ecce10282a5fba46a899
KonstantinSviridov <ksviridov@gmail.com>
2017-06-14 03:12:04 +0700

    debugging

--- src/raml1/test/scripts/platformTests.ts
+++ src/raml1/test/scripts/platformTests.ts
@@ -35,48 +35,4 @@ function operate(){
     testUtils.contributeTheStorage(repoDir, ["trigger.txt"], `TARGET_BRANCH=${parserBranch}`, false);
 }
 
-function pluginBranch(pluginName:string,folderOrDescriptor:string, rootFolder?:string):string{
-
-    let descriptor:string = folderOrDescriptor;
-    if(fs.lstatSync(descriptor).isDirectory()){
-        descriptor = path.resolve(descriptor,"workspace.json");
-    }
-    if(!fs.existsSync(descriptor)){
-        return null;
-    }
-    rootFolder = rootFolder || path.dirname(descriptor);
-    let packagejsonfile = path.resolve(rootFolder,"package.json");
-    if(!fs.existsSync(packagejsonfile)){
-        console.log("no package.json");
-        return null;
-    }
-    try {
-        let packagejson = JSON.parse(fs.readFileSync(packagejsonfile,"utf8"));
-        if(packagejson.name != pluginName){
-            return null;
-        }
-    }
-    catch (e){
-        console.log(e);
-        return null;
-    }
-    console.log("rootFolder: " + rootFolder);
-    fs.readdirSync(rootFolder).forEach(x=>{
-       console.log(x);
-    });
-    let gitFolder = path.resolve(rootFolder,".git");
-    if(fs.existsSync(gitFolder)){
-        console.log("gitFolder: " + gitFolder);
-        fs.readdirSync(gitFolder).forEach(x=>{
-            console.log(x);
-        });
-    }
-    let branchName = gitBranch.sync(rootFolder);
-    if(typeof branchName == "string") {
-        return branchName;
-    }
-    console.log("invalid branch " + branchName);
-    return null;
-}
-
 operate();
