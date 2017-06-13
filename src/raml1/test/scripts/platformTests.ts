4666145df40bbb268dd6a0b1d9f8fc53714483ba
KonstantinSviridov <ksviridov@gmail.com>
2017-06-14 01:20:51 +0700

    script fix

--- src/raml1/test/scripts/platformTests.ts
+++ src/raml1/test/scripts/platformTests.ts
@@ -3,12 +3,18 @@ import path = require("path");
 import fs = require("fs");
 
 function operate(){
+
     let commitId = process.env.TRAVIS_COMMIT;
     if (!commitId) {
         return;
     }
 
     let rootDir = testUtils.rootDir(__dirname);
+    let parserBranch = testUtils.pluginBranch("raml-1-parser",rootDir);
+    if(!parserBranch){
+        return;
+    }
+
     testUtils.setSSHUrl(rootDir);
     testUtils.setGitUser(rootDir);
     let homeDir = path.resolve(rootDir, "../../../");
@@ -24,7 +30,7 @@ function operate(){
     let repoDir = path.resolve(wsDir, "PlatformComparisonScript");
     testUtils.insertDummyChanges(repoDir);
 
-    testUtils.contributeTheStorage(rootDir, ["trigger.txt"], `TARGET_COMMIT=${commitId}`, true);
+    testUtils.contributeTheStorage(rootDir, ["trigger.txt"], `TARGET_BRANCH=${parserBranch}`, false);
 }
 
 operate();
