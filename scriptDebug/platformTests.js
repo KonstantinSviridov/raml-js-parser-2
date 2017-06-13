41c396d77842c200541a12713a9104b60372a787
KonstantinSviridov <ksviridov@gmail.com>
2017-06-14 03:12:51 +0700

    debugging

--- scriptDebug/platformTests.js
+++ scriptDebug/platformTests.js
@@ -1,7 +1,6 @@
 "use strict";
 var testUtils = require("raml-1-parser-test-utils");
 var path = require("path");
-var fs = require("fs");
 var gitBranch = require("git-branch");
 function operate() {
     var commitId = process.env.TRAVIS_COMMIT;
@@ -9,7 +8,7 @@ function operate() {
         return;
     }
     var rootDir = testUtils.rootDir(__dirname);
-    var parserBranch = pluginBranch("raml-1-parser", rootDir);
+    var parserBranch = testUtils.pluginBranch("raml-1-parser", rootDir);
     if (!parserBranch) {
         console.warn("No parser branch has been detected");
         return;
@@ -23,52 +22,6 @@ function operate() {
     testUtils.setSSHUrl(repoDir);
     testUtils.insertDummyChanges(repoDir);
     testUtils.contributeTheStorage(repoDir, ["trigger.txt"], "TARGET_BRANCH=" + parserBranch, false);
-}
-function pluginBranch(pluginName, folderOrDescriptor, rootFolder) {
-    var descriptor = folderOrDescriptor;
-    if (fs.lstatSync(descriptor).isDirectory()) {
-        descriptor = path.resolve(descriptor, "workspace.json");
-    }
-    if (!fs.existsSync(descriptor)) {
-        return null;
-    }
-    rootFolder = rootFolder || path.dirname(descriptor);
-    var packagejsonfile = path.resolve(rootFolder, "package.json");
-    if (!fs.existsSync(packagejsonfile)) {
-        console.log("no package.json");
-        return null;
-    }
-    try {
-        var packagejson = JSON.parse(fs.readFileSync(packagejsonfile, "utf8"));
-        if (packagejson.name != pluginName) {
-            return null;
-        }
-    }
-    catch (e) {
-        console.log(e);
-        return null;
-    }
-    console.log("rootFolder: " + rootFolder);
-    fs.readdirSync(rootFolder).forEach(function (x) {
-        console.log(x);
-    });
-    var gitFolder = path.resolve(rootFolder, ".git");
-    if (fs.existsSync(gitFolder)) {
-        console.log("gitFolder: " + gitFolder);
-        fs.readdirSync(gitFolder).forEach(function (x) {
-            console.log(x);
-        });
-    }
-    var branchName = gitBranch.sync(rootFolder);
-    if (typeof branchName == "string") {
-        return branchName;
-    }
-    branchName = process.env.TRAVIS_BRANCH;
-    if (typeof branchName == "string") {
-        return branchName;
-    }
-    console.log("invalid branch " + branchName);
-    return null;
 }
 operate();
 //# sourceMappingURL=platformTests.js.map
